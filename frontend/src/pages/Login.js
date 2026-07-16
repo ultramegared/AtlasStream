fce74a6b7d243ab0a4a50d7dd35a59a1dd58d03a
ultramegared <301172501+ultramegared@users.noreply.github.com>
2026-07-16 09:10:48 -0500

    feat(frontend): create Login page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,29 @@
+export function LoginPage() {
+  return `
+    <section class="login-page">
+      <h1>Iniciar sesión</h1>
+
+      <input
+        type="email"
+        placeholder="Correo electrónico"
+      />
+
+      <input
+        type="password"
+        placeholder="Contraseña"
+      />
+
+      <button id="loginButton">
+        Iniciar sesión
+      </button>
+
+      <p>
+        ¿No tienes una cuenta?
+      </p>
+
+      <button id="registerButton">
+        Crear cuenta
+      </button>
+    </section>
+  `;
+}
