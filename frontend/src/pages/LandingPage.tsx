eede8fb055f7f6c1a68391ca0a787d7013cbc82f
ultramegared <301172501+ultramegared@users.noreply.github.com>
2026-08-04 00:30:25 -0500

    git add .  git commit -m "fix(landing): rebuild landing page structure"  git push

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -11,13 +11,14 @@
  */
 
 import LandingNavbar from "../components/landing/LandingNavbar";
-
 import LandingHero from "../components/landing/LandingHero";
 import LandingTrending from "../components/landing/LandingTrending";
 import LandingFeatures from "../components/landing/LandingFeatures";
 import LandingPlans from "../components/landing/LandingPlans";
 import LandingFAQ from "../components/landing/LandingFAQ";
 import LandingFooter from "../components/landing/LandingFooter";
+
+import "../styles/landing.css";
 
 function LandingPage() {
 
